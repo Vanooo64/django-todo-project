53cbc83d022e7426a0c49f0870bd15c0ee09ebac
Vano <glazglojon@gmail.com>
2025-03-13 14:46:33 +0200

    create list ordering for order.html

--- todo_manager/orders/urls.py
+++ todo_manager/orders/urls.py
@@ -1,13 +1,9 @@
 from django.urls import path
 from django.views.generic import TemplateView
 
+from . import views
 app_name = 'orders'
 
 urlpatterns = [
-    path("",
-         TemplateView.as_view(
-             template_name="orders/index.html"
-         ),
-         name='index'
-    ),
+    path("", views.index_view, name='index'),
 ]
